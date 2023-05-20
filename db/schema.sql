5482e66016d27459a09a0c9f837eaeaa6e29beae
LatinLady <marielblyden@hotmail.com>
2023-05-20 02:33:36 -0400

    fixing bug

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,21 @@
 CREATE TABLE department (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+  id INT PRIMARY KEY,
+  name VARCHAR(50),
+  location VARCHAR(50)
 );
+
 
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
+    title VARCHAR(50),
     salary DECIMAL,
     department_id INTEGER
 );
 
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER
 );
