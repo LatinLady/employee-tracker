aa525f421b39e1e7a25084747649c3ab28f6cfe2
LatinLady <marielblyden@hotmail.com>
2023-05-20 00:56:09 -0400

    fixing files

--- db/schema.sql
+++ db/schema.sql
@@ -1,46 +1,19 @@
-DROP DATABASE IF EXISTS employee_info_db;
-
-CREATE DATABASE employee_info_db;
-
-USE employee_info_db;
-
 CREATE TABLE department (
-
-id INT NOT NULL AUTO_INCREMENT,
-
-name VARCHAR(30) NOT NULL,
-
-PRIMARY KEY(id)
-
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
-
 CREATE TABLE role (
-
-id INT NOT NULL AUTO_INCREMENT,
-
-title VARCHAR(30) NOT NULL,
-
-salary DECIMAL(10,2) NOT NULL,
-
-department_id INT NOT NULL,
-
-PRIMARY KEY (id)
-
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER
 );
 
 CREATE TABLE employee (
-
-id INT NOT NULL AUTO_INCREMENT,
-
-first_name VARCHAR(30) NOT NULL,
-
-last_name VARCHAR(30) NOT NULL,
-
-role_id INT NOT NULL,
-
-manager_id INT,
-
-PRIMARY KEY (id)
-
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,26 @@
-INSERT INTO department (name)
-VALUES ("Security"), ("Arc Reactor"), ("Engineering"), ("Hospitality"), ("Leadership");
+-- Inserts names of departments into department table
+INSERT INTO department
+  (name)
+VALUES
+  ('Engineering'),
+  ('Sales'),
+  ('Finance'),
+  ('Legal');
 
-INSERT INTO role (title, salary, department_id)
-VALUE ("Head of Security", 25000.00, 2), ("Scientist", 600000.00, 3), ("Engineer", 600000.00, 4), ("Butler", 200000.00, 1), ("CEO", 800000.00, 5);
+-- Inserts roles of employee into role table
+INSERT INTO role
+  (title, salary, department_id)
+VALUES
+  ('Software Engineer', 85000, 1),
+  ('Salesperson', 75000, 2),
+  ('Accountant', 125000, 3),
+  ('Lawyer', 200000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUE ("Happy", "Hogan", 1, 3), ("Anton", "Vanko", 1, 1), ("Tony", "Stark", 3, 2), ("Steve", "Quinn", 5, 2), ("Pepper", "Potts", 5, 2);
+-- Inserts employee information into employee table
+INSERT INTO employee
+  (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Juan', 'Garcia', 1, 4),
+  ('Jonathan', 'Villcapoma', 2, 3),
+  ('Jesus', 'Meraz', 3, 1),
+  ('Estefany', 'Munoz', 4, 5);
