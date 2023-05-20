92aecba156ddabcf494e3321735c0ee9039a9c33
LatinLady <marielblyden@hotmail.com>
2023-05-20 02:56:42 -0400

    fixing bug

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
-const mysql = require('mysql2');
+const mysql = require('mysql');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: 'localhost',
+    host: 'localhost 3001',
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
