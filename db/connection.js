24572d87342591b9bf12746da6b4d71fcee095a3
LatinLady <marielblyden@hotmail.com>
2023-05-21 10:35:23 -0400

    create cfonts

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,24 @@
 const mysql = require('mysql');
-//require('dotenv').config();
+const cfonts = requirer('cfonts');
+require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: '3001',
-    // Your MySQL username,
+    host: 'localhost',
+    port: 3001,
     user: 'root',
-    // Your MySQL password
     password: 'windows',
     database: 'employee_tracker_db',
+});
+
+// Function to start the application
+cfonts.say('Employee Tracker', {
+    font : 'block',
+    align : 'center',
+    color : 'pink',
+    background : 'transparent',
+    letterSpacing : 1,
+    lineHeight : 1,
+    space : true,
 });
 
 module.exports = db;
